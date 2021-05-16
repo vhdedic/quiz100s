eab7d35ffa8f4608d8275ad4fe66468924b41fa1
vhdedic <vdedic@gmail.com>
2021-05-16 15:19:25 +0200

    Edit src/Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,7 +3,11 @@ import React from 'react';
 class Quiz extends React.Component {
 	render() {
 		return (
-			<h1>Quiz 100 seconds</h1>
+			<div>
+				<h1>Quiz 100 seconds</h1>
+				<p>Press Start.</p>
+				<button>Start</button>				
+			</div>
 		)
 	}
 }
