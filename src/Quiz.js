c7389821e7b58d939e95b6ab9e87085401b1070a
vhdedic <vdedic@gmail.com>
2021-05-17 15:07:36 +0200

    Create src/components/QuizStart.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from 'react-router-dom';
+import QuizStart from './components/QuizStart';
 
 class Quiz extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>Quiz 100 seconds</h1>
-				<p>Press Start.</p>
-				<button>Start</button>				
-			</div>
+			<Router>
+				<div>
+					<h1>Quiz 100 seconds</h1>
+					<Switch>
+						<Route path="/">
+							<QuizStart />							
+						</Route>
+					</Switch>
+				</div>
+			</Router>
 		)
 	}
 }
---
+++ src/components/QuizStart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class QuizStart extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Press Start.</p>
+				<button>Start</button>				
+			</div>
+		)
+	}
+}
+
+export default QuizStart;
