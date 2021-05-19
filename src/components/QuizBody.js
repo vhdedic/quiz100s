a0af39a54a1fae200c2db0d81092405adf4365e7
vhdedic <vdedic@gmail.com>
2021-05-19 15:22:05 +0200

    Get data from json file

--- src/components/QuizBody.js
+++ src/components/QuizBody.js
@@ -2,10 +2,15 @@ import React from 'react';
 import shuffle from 'shuffle-array';
 import questions from './json/questions.json';
 
+const data = shuffle(questions);
+
 class QuizBody extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {time: 100};
+    this.state = {
+      time: 100,
+      score: 0
+    };
   }
 
   componentDidMount() {
@@ -25,16 +30,36 @@ class QuizBody extends React.Component {
   }
 
   render() {
+    const position = data[this.state.score]
     return (
       <div>
-        <p>Question?</p>
-        <input type="button" className="button" value="A" />
-        <input type="button" className="button" value="B" />
-        <input type="button" className="button" value="C" />
-        <input type="button" className="button" value="D" />
+        <p>{position.question}</p>
+        <input 
+          type="button" 
+          className="button" 
+          value={position.answer_a}
+        />
+        <input
+         type="button"
+         className="button"
+         value={position.answer_b} 
+        />
+        <input 
+          type="button" 
+          className="button" 
+          value={position.answer_c} 
+        />
+        <input 
+          type="button" 
+          className="button" 
+          value={position.answer_d} 
+        />
         <p>
-            Remaining time: {this.state.time}s
-          </p>
+          Remaining time: {this.state.time}s
+        </p>
+        <p>
+          Score: {this.state.score}
+        </p>
       </div>
     )
   }
